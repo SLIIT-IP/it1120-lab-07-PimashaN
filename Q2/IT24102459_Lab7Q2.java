16f8e7af0807c37c3c4c1286f8a8e84c9c93d43b
PimashaN <it24102459@my.sliit.lk>
2024-10-01 23:17:51 +0530

    IT24102459_Lab7Q2

---
+++ Q2/IT24102459_Lab7Q2.java
@@ -0,0 +1,10 @@
+public class IT24102459Lab7Q2 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println(); 
+        }
+    }
+}
